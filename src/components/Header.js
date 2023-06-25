6c2ed59b29454c770ac27a783d7c8e1910456470
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-06-24 19:15:51 -0500

    Added initiation component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,26 +5,26 @@ import { Link } from "react-router-dom"
 export const Header = () => {
   return (
     <header>
-      <div className="flex place-content-center gap-4 md:justify-around sm:justify-around h-[100px] sm:h-[90px] md:h-[90px] items-center 
+      <div className="grid sm:flex md:flex place-content-center justify-items-center gap-4 
+      md:justify-around sm:justify-around h-[130px] sm:h-[90px] md:h-[90px] 
       flex-wrap box-border shadow-md bg-yellow-500">
         <div className="flex gap-1">
-          <Link href="#">
-            <div className='w-[40px] m-1 shadow-lg'>
+          <Link to="/">
+            <div className='w-[30px] sm:w-[40px] md:w-[40px] m-1 shadow-lg'>
               <img src={LightBlue} alt=""/>
             </div>
           </Link>
-          <h1 className="text-2xl font-bold underline shadow-sm">Mg-ars store</h1>
+          <h1 className="text-xl sm:text-2xl md:text-2xl font-bold underline shadow-sm">Mg-ars store</h1>
         </div>
-        <ul className="flex gap-2 text-[14px] sm:text-[16px] md:text-[16px] 
-        font-bold">
+        <ul className="flex gap-2 sm:py-2 text-[14px] sm:text-[16px] md:text-[16px] font-bold">
           <li>
-            <Link to="/" className="hover:text-sky-950">INICIO</Link>
+            <Link to="/" className="hover:text-sky-900">INICIO</Link>
           </li>
           <li>
-            <Link to="/products" className="hover:text-sky-950">PRODUCTOS</Link>
+            <Link to="/products" className="hover:text-sky-900">PRODUCTOS</Link>
           </li>
         </ul>
-        <div className="relative cursor-pointer">
+        <div className="relative cursor-pointer sm:py-2">
           <box-icon name="cart"></box-icon>
           <span className="absolute w-[18px] rounded-xl text-center top-1 left-6 bg-red-700 text-white">0</span>
         </div>
