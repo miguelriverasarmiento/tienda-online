9623c2ffa477db9ad14b864ec1ad6cae11bd9780
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-06-28 08:51:51 -0500

    Added footer with responsive desing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>mg-ars store 2023</div>
+    <div className="bg-headerpic bg-cover bg-center bg-no-repeat mt-32 h-[100px] text-orange-600 grid 
+    place-items-center text-[13px] sm:text-[15px] md:text-[17px]">
+
+      <div className="text-center">
+        <p>&copy; mg-ars store 2023</p>
+      </div>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
       "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'headerpic': "url('https://t3.ftcdn.net/jpg/04/05/44/24/360_F_405442455_M6iY7q8Q1uEKHSdtqFjYNLvnreMPpJn2.jpg')",
+        },
+      },
     },
     plugins: [],
 }
