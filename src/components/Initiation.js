462dc1b228b5b3c4da709afb25080f244c509ac6
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-08-11 11:30:27 -0500

    Add desing measures

--- src/components/Initiation.js
+++ src/components/Initiation.js
@@ -10,15 +10,15 @@ export const Initiation = () => {
   const { logout } = useAuth0();
 
   if (isLoading) {
-    return <div className='ml-10 md:ml-36 mt-10 min-h-screen text-yellow-800 sm:text-lg md:text-lg 
-    font-extralight'>Loading...</div>;
+    return <div className='mt-10 mx-5 sm:mx-12 md:mx-20 lg:mx-28 xl:mx-40 2xl:mx-44 text-yellow-800 
+    sm:text-lg md:text-lg font-extralight'>Loading...</div>;
   }
 
   return (
-    <div className='mx-5 md:mx-20'>
+    <div className='mx-5 sm:mx-12 md:mx-20 lg:mx-28 xl:mx-40 2xl:mx-44'>
       {isAuthenticated ? (
         <>
-          <div className='grid grid-rows-1 gap-3 w-[200px] sm:w-[400px] mt-10 lg:pl-4 xl:pl-16 2xl:pl-32'>
+          <div className='grid grid-rows-1 gap-3 w-[200px] sm:w-[400px] mt-10'>
             <div className='flex flex-row gap-1'>
               <img src={user.picture} alt={user.name} className='w-[30px] rounded-full'/>
               <p className='w-[220px] text-sm font-light text-yellow-950 py-1'>{user.name}</p>
@@ -33,7 +33,7 @@ export const Initiation = () => {
         </>
         ) : 
         <>      
-          <div className='xl:justify-center w-[200px] sm:w-[400px] sm:flex md:flex gap-3 mt-10'>
+          <div className='w-[200px] sm:w-[400px] sm:flex md:flex gap-3 mt-10'>
             <div className='text-sm px-4 py-2 border-2 border-amber-200 bg-amber-300 
             hover:bg-yellow-300 rounded-md'>
               <button onClick={() => loginWithRedirect()}>
@@ -49,14 +49,14 @@ export const Initiation = () => {
           </div>
         </>
         }
-      <div className='grid grid-rows-1 gap-5 lg:justify-center'>
+      <div className='grid grid-rows-1 gap-5'>
         <div className='mt-20 font-light text-sm md:text-base'>
-          <p>La música expresa lo que no puede ser dicho 
+          <p className='w-[100%]'>La música expresa lo que no puede ser dicho 
             y aquello sobre lo que es imposible permanecer en silencio.</p>
           <p>−Victor Hugo</p>
         </div>
         <div className="my-10">
-          <img src={Portada} alt="inicio" />
+          <img src={Portada} alt="inicio" className="w-[100%]"/>
         </div>
       </div>
     </div>
